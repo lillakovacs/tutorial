54e7c1103c378dd92aeb16e29c226a4a203899a4
lillakovacs <kovacs.lilla@ppk.elte.hu>
2019-09-20 11:32:00 +0200

    trial

---
+++ x.R
@@ -0,0 +1,9 @@
+git config --global user.email "lilla.kovacs@ppk.elte.hu"
+git config --global user.name "Lilla Kovacs"
+
+install.packages("ggplot2")
+install.packages("Rtools")
+library(ggplot2)
+
+USArrests
+#trial script 
